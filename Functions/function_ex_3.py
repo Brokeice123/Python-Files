9c1546a54960c2f310d5b7af6e35a3b029fc3bdd
eMobilis <you@example.com>
2023-02-13 12:35:18 +0300

    functions with parameters

--- Functions/function_ex_3.py
+++ Functions/function_ex_3.py
@@ -1,5 +1,8 @@
-def age_months():
-    age = int(input("Enter your age"))
-    months = age*12
-    print(f"You have lived for {months} months")
-age_months()
+try:
+    def age_months():
+        age = int(input("Enter your age"))
+        months = age*12
+        print(f"You have lived for {months} months")
+    age_months()
+except:
+    print("Not a number")

d3d6ab0ade5eb02d10cc708afd55357ed86b66fc
eMobilis <you@example.com>
2023-02-14 12:51:16 +0300

    Modification of FizzBuzz to a dynamic version

--- Quiz/main.py
+++ Quiz/main.py
@@ -1,4 +1,7 @@
-for number in range(100):
+num1 = int(input("Enter first number"))
+num2 = int(input("Enter second number"))
+
+for number in range(num1, num2):
     if number % 3 == 0 and number % 5 == 0:
         print(number, "FizzBuzz")
     elif number % 3 == 0:
@@ -6,3 +9,6 @@ for number in range(100):
     elif number % 5 == 0:
         print(number, "Buzz")
     print(number)
+
+    # the == is a modulus. It checks if a number has a remained of not
+
